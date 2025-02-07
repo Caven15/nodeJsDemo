4d73b0c00b46635bc4649c5e12eac79e13d6eac3
Caven_15 <cavnenditch15@gmail.com>
2025-02-07 15:00:34 +0100

    base repo service controller (mauvaise pratique)

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Importation du module HTTP de Node.js pour l création de notre serveur
 import http from "http";
 
+// Repository
 let livres = [
     {
         id : 1,
@@ -16,35 +17,29 @@ let livres = [
     }
 ];
 
-// http.createServer => Crée un serveur qui éctoute les requêtes entrantes.
-// Le serveur tourne en boucle et exécute la fonction callback chaque fois qu'une requette http arrive.
-const server =  http.createServer((req,res) => {
-    // req => Contient les information de la requête http envoyé par le client.
-        // permet d'acceder 
-            // Url 
-            // Verb => GET POST DELETE PATCH ect....
-            // Les en-têtes http (headers)
-            // Les données que celle-ci contient (creation, mise à jour)
 
-    // res => la réponse pour renvoyer les infos au client
-        // permet d'acceder 
-            // Définir le code de statut (ex 200 => ok || 404 not found)
-            // Définition du type de retour ( text / json / autre...)
-            // Envoyer les donnée au client  en json ou en texte
+// Service
+const getAllBookService = () => {
+    return livres;
+};
+
+
+// Controller 
+const getAllBookController = (req, res) => {
+    const livres = getAllBookService();
+    res.writeHead(200, { "Content-Type": "application/json"});
+    res.end(JSON.stringify(livres))
+};
+
+// Routage manuel
+const server =  http.createServer((req,res) => {
 
     if (req.method === "GET" && req.url === "/livres"){
-        // Si l'utilisateur demande GET /livres
-
-        // Définition du statut de la réponse http (200 = OK)
-        res.writeHead(200, {"content-type" : "application/json"});
-
-        // Envoi des données sous forme de json
-        res.end(JSON.stringify(livres))
-        console.log("Récupération de tout les livres...");
+        getAllBookController(req,res);
     }
-})
+});
 
 // Le serveur écoute les requêtes sur le 3000 et tourne en boucle indéfiniment
 server.listen(3000, () => {
     console.log("Serveur démarré sur http://localhost:3000");
-})
+});
